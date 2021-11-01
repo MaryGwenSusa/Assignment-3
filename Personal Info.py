62f2ff95a6f480c621e606d5a459434034469208
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-01 22:04:07 +0800

    Welcome function

--- Personal Info.py
+++ Personal Info.py
@@ -21,3 +21,30 @@ name = obtainName()
 age = obtainAge()
 address = obtainAddress()
 intro(name, age, address)
+
+
+# another version
+def header():
+    print(f"Personal Identification")
+
+def obtainPersonalInfo():
+    yourName = input("Name: ")
+    yourAge = int(input("Age: "))
+    yourAddress = input("Address: ")
+    return print (f"Hi, my name is {yourName}. I am {yourAge} years old and I live in {yourAddress}.")
+
+header()
+obtainPersonalInfo()
+
+# another version
+def header():
+    print(f'Personal Information')
+
+def obtainPersonalInfo(name, age, address):
+    return print(f'Hi, my name is {name}. I am {age} years old and I live in {address}.')
+
+header()
+yourName = input("Name:")
+yourAge = int(input("Age:"))
+yourAddress = input("Address:")
+obtainPersonalInfo(yourName, yourAge, yourAddress)
