623b39d4fbf42ec13c7bb016a61621e26318cea9
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-01 21:50:56 +0800

    Call functions

--- Personal Info.py
+++ Personal Info.py
@@ -5,3 +5,19 @@ def obtainName():
     yourName = input("Name: ")
     return yourName
 
+def obtainAge():
+    yourAge = int(input("Age: "))
+    return yourAge
+
+def obtainAddress():
+    yourAddress = input("Address: ")
+    return yourAddress
+
+def intro(nameX, ageX, addressX):
+    print (f"Hi, my name is {nameX}. I am {ageX} years old and I live in {addressX}.")
+
+header()
+name = obtainName()
+age = obtainAge()
+address = obtainAddress()
+intro(name, age, address)
