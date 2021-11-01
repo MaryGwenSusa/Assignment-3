046a217e362239a879ada17b5a4be39023a83111
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-01 22:25:11 +0800

    Call functions

---
+++ AppleCash.py
@@ -0,0 +1,20 @@
+def openStatement():
+    print(f"Budget for Comfort Food: Apple")
+
+def cashMonitor():
+    yourWallet = float(input('Your Current Money:\n> '))
+    return yourWallet
+
+def fluctuationPrice():
+    priceOfApple = float(input('How much is the apple?\n> '))
+    return priceOfApple
+
+def computation(money, price):
+    maximumNumberOfApples = int(money/price)
+    remainingMoney = float(money%price)
+    return print(f"You can buy {maximumNumberOfApples} and your change is {remainingMoney:.2f}.")
+
+openStatement()
+currentMoney = cashMonitor()
+currentPrice = fluctuationPrice()
+computation(currentMoney, currentPrice)
